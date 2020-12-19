46889531c8ad9820ed96c082476d1ac283ddf6be
YUNUSGOK <ynsmrgok@gmail.com>
2020-12-19 21:15:25 +0300

    .

--- server/server.py
+++ server/server.py
@@ -112,11 +112,14 @@ def udpServer(UDP_SERVER_PORT , packet_corruption_ratio = 0, delaying_ratio = 0 
             if  (decodedMessage["index"] == expected and check == True): #no bit error and expected packet arrived
                 if(decision(delaying_ratio/100)): # delay will occur with given probablity
                     time.sleep(delay_time)
-                expected += 1 # expected packet number will be inceremented 
+
                 sendedTimes.append(decodedMessage["sendedTime"]) # sended time will be saved for further calculations
                 recievedTimes.append(recievedTime)  # recieved time will be saved for further calculations
                 UDPServerSocket.sendto(responseMessage(1,expected), address) #ACK will be sended
+
                 recievedFileData += decodedMessage["data"]  # recieved data will be saved
+                expected += 1 # expected packet number will be inceremented 
+                
             else:
                 (UDPServerSocket.sendto(responseMessage(0, expected), address) ) #ACK will be sended
         except:
